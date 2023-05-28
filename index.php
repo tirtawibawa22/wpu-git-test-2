890db41b93a32fc68540320f62617e8969417188
webprogrammingunpas <wpunpas@gmail.com>
2023-05-28 17:38:04 +0800

    menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat autem eaque aspernatur sint! Ea consectetur a, aliquam autem quod dolor laborum reprehenderit sit officia libero commodi fuga, voluptatibus dolores asperiores.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
